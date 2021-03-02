c4a928ef159a1b7626f51511be9d9329d2b355f7
CheoR <cheo.roman+github@gmail.com>
2021-03-02 10:47:20 -0600

    feat: add project scaffolding & db

--- src/components/KandyKorner.jsx
+++ src/components/KandyKorner.jsx
@@ -1,9 +1,13 @@
 import React from "react"
+import { LocationList } from "./locations/LocationList"
+import { LocationProvider } from "./locations/LocationProvider"
 
 export const KandyKorner = ( props ) => (
   <>
   <h1>Kandy Korner</h1>
   <div><p>Hi</p></div>
+  <LocationProvider>
+    <LocationList />
+  </LocationProvider>
   </>
 ) // KandyKorner
- 
